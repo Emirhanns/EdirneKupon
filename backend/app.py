0046a6a5ea8fc60c86642c5a6296747df5f45c5c
Emirhan Çetin <ctn.emrhn0@gmail.com>
2024-12-26 16:24:55 +0300

    update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,13 @@
 # app.py
 
-from flask import Flask
+from flask import Flask, send_from_directory
 from utils.db import init_db
 from flask_cors import CORS
 from routes.auth_routes import auth_routes
 from routes.admin_routes import admin_routes 
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='build', static_url_path='')
 
 CORS(app)  # Cross-Origin Resource Sharing ayarları
 
@@ -18,11 +18,15 @@ init_db(app)
 app.register_blueprint(auth_routes)
 app.register_blueprint(admin_routes)
 
+# React yönlendirmelerini desteklemek için catch-all route
+@app.route('/')
+@app.route('/<path:path>')
+def serve(path=None):
+    return send_from_directory(app.static_folder, 'index.html')
 
 app.config['DEBUG'] = False  # Debug özelliğini kapat
 app.config['TESTING'] = False  # Testing özelliğini kapat
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')  # Güvenli bir anahtar
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
